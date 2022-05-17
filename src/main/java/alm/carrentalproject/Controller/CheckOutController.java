aa19b8b4d3a8241739da61eae0d79cf509142f2b
Alidata <alidata127@gmail.com>
2022-05-17 19:44:35 -0400

    Update CheckOutController.java

--- src/main/java/alm/carrentalproject/Controller/CheckOutController.java
+++ src/main/java/alm/carrentalproject/Controller/CheckOutController.java
@@ -4,27 +4,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 
 @Controller
-public class CheckOutController {
+public class CheckoutController {
 
-    @Value("${STRIPE_PUBLIC_KEY}")
-    private String stripePublicKey;
+    @Value("${stripe_public_key}")
+    String stripePublicKey;
 
-    // http://localhost:8080/checkout?userId=1&&amount=40
+        // http://localhost:8080/checkout?systemUserId=1&&amount=40 //
     @GetMapping("/checkout")
-    public ModelAndView checkOut(Model model, @RequestParam(required = true) String userId,
-                                        @RequestParam(required = true) int amount) {
+    public String checkout(Model model, @RequestParam(required = true) String systemUserId,
+                           @RequestParam(required = true) Integer amount) {
+        model.addAttribute("amount", amount*100);
+        model.addAttribute("stripePublicKey", stripePublicKey);
+        model.addAttribute("currency", "USD");
+        model.addAttribute("systemUserId", systemUserId);
+        return "checkout";
 
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("amount", amount*100);
-        mav.addObject("stripePublicKey", stripePublicKey);
-        mav.addObject("currency", "CAD");
-        mav.addObject("userID", userId);
-
-        return mav;
     }
 }

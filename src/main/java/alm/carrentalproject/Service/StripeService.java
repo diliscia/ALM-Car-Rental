f2171d19766cf419ebbc9fcbc1f01a3cb8910888
Quynh-Ly Do <quynhly.do@gmail.com>
2022-05-18 18:03:50 -0400

    add list of users and update front page, link list of users to booking page

--- src/main/java/alm/carrentalproject/Service/StripeService.java
+++ src/main/java/alm/carrentalproject/Service/StripeService.java
@@ -17,7 +17,6 @@ import java.util.Map;
 @Service
 public class StripeService {
 
-//    @Value()
     private String secretKey = "sk_test_51Kyev5D6hSMxeUqVL4Cluwn4bewVKGD43bRftb5WFpLNabY0B0Dsm6b8P36ZLnx1Zx04i1lZJ405Vn5AHsCK9iQz00oncvVoSM";
 
     @PostConstruct
--- src/main/java/alm/carrentalproject/Service/UserService.java
+++ src/main/java/alm/carrentalproject/Service/UserService.java
@@ -1,6 +1,7 @@
 package alm.carrentalproject.Service;
 
 import alm.carrentalproject.Entity.User;
+import alm.carrentalproject.Entity.Vehicle;
 import alm.carrentalproject.Repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -61,5 +64,12 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    public ModelAndView listUsers() {
+        ModelAndView mav = new ModelAndView("listUser");
+        List<User> userList = userRepository.findAll();
+        mav.addObject("userList", userList);
+        return mav;
     }
 }

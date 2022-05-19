cc0262d053145341a5e87170559fe30872753053
Marcelo Di Liscia <diliscia@gmail.com>
2022-05-19 13:49:24 -0400

    updated format in general
    updated format in general

--- src/main/java/alm/carrentalproject/Controller/WebController.java
+++ src/main/java/alm/carrentalproject/Controller/WebController.java
@@ -3,11 +3,20 @@ package alm.carrentalproject.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class WebController {
     @GetMapping("/check")
     public String home(Model model) {
         return "checkout";
+    }
+
+    @GetMapping({"", "/index"})
+    public ModelAndView viewHomePage() {
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
     }
 }

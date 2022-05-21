9639c1be63e9afe6e4e2936e32071712a50490a9
Quynh-Ly Do <quynhly.do@gmail.com>
2022-05-21 04:12:29 -0400

    update chart

--- src/main/java/alm/carrentalproject/Controller/StatisticsController.java
+++ src/main/java/alm/carrentalproject/Controller/StatisticsController.java
@@ -2,9 +2,13 @@ package alm.carrentalproject.Controller;
 
 import alm.carrentalproject.Entity.Statistics;
 import alm.carrentalproject.Repository.StatisticsRepository;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -23,11 +27,26 @@ public class StatisticsController {
         return mav;
     }
 
-    @GetMapping("/admin/statistics1")
-    public ModelAndView showStatistics1(){
-        ModelAndView mav=new ModelAndView("statistics1");
-        List<Statistics> statistics=statisticsRepository.getAll();
-        mav.addObject("statistics",statistics);
-        return mav;
+    @GetMapping("/admin/chart")
+    public String showChart() {
+        return "statistic";
     }
+
+    @RequestMapping("/admin/statistic")
+    @ResponseBody
+    public String showStatistics1(){
+        List<Statistics> statistics = statisticsRepository.getAll();
+        JsonArray jsonDay = new JsonArray();
+        JsonArray jsonNumberOfBookings = new JsonArray();
+        JsonObject json = new JsonObject();
+        statistics.forEach(data -> {
+            jsonDay.add(data.getDate());
+            jsonNumberOfBookings.add(data.getNumber());
+        });
+        json.add("date", jsonDay);
+        json.add("numberOfBookings", jsonNumberOfBookings);
+        return json.toString();
+    }
+
+
 }
